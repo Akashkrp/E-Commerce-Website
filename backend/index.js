d9375c1bd320e5a8d251977397a6ad7faeb211b1
Akashkrp <kumarak2061@gmail.com>
2025-04-10 18:39:57 +0530

    Add user controller, authMiddleware JWT

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,19 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
  
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config();
+dotenv.config(); 
 const port = process.env.PORT || 5000;
 
 connectDB();
-const app = express();
+const app = express(); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+ 
+app.use("/api/users", userRoutes); 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
